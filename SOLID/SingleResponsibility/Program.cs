51a22565845002263981fbd2215f530433073f1e
Le Duy Lin <leduylin23@gmail.com>
2023-05-12 08:34:37 +0700

    finish implementing an example applying Single Responsiblity principle

---
+++ SOLID/SingleResponsibility/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace SingleResponsibility
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate a bug");
+            var persistance = new Persistance();
+            var fileName = @"D:\journal.txt";
+            persistance.SaveToFile(journal, fileName, true);
+            Process.Start(new ProcessStartInfo { FileName = fileName, UseShellExecute = true });
+        }
+    }
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+
+    public class Persistance
+    {
+        public void SaveToFile(Journal journal, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+        }
+    }
+}
