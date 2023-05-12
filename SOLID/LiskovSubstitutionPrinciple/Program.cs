b2304d7af95d08cc0dffaa28354e24cc99a00b94
Le Duy Lin <leduylin23@gmail.com>
2023-05-12 14:15:47 +0700

    initilize example of LiskovSubstitutionPrinciple

---
+++ SOLID/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+    public class Program
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(2, 3);
+            Console.WriteLine($"{rectangle} has area {Area(rectangle)}");
+        }
+    }
+}
---
+++ SOLID/LiskovSubstitutionPrinciple/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle() { }
+
+        public Rectangle(int width, int height) { Width = width; Height = height; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)} : {Height}";
+        }
+    }
+}
